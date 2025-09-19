7b00ed64ebd20e27445240bb885a8983886558e2
somnathshingade <somnath.shingade@innomotics.com>
2025-09-19 10:16:13 +0530

    Add files via upload

---
+++ 01_Basic.py
@@ -0,0 +1,29 @@
+#name = input("Enter your name: ")
+#print("Hello, " + name)
+
+#age = int(input("Enter your age: "))
+#if age >= 18:
+  #  print("You are an adult.")
+#else:
+ #   print("You are a minor.")
+
+
+#for i in range(5):
+ #   print("Loop number", i)
+
+
+#names = ["Amit", "Ravi", "Priya", "Neha", "Raj"]
+#for name in names:
+#    print("Hello", name)
+
+#numbers = ["20", "30", "40", "50", "60"]
+#for number in numbers:
+#    print("Hello", number)
+
+# for i in range(1, 10, 2):
+#    print("Step:", i)
+
+def greet(name):
+   return f"Hello, {name}!"
+
+print(greet("Amit"))
